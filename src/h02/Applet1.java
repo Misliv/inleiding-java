f0c2517176aa8425b8c3517acb74bbfddfec1fae
Misliv <may.hoang@student.rocvf.nl>
2020-09-17 23:08:35 +0200

    added applet code, classnotfoundexception

---
+++ src/h02/Applet1.java
@@ -0,0 +1,15 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Applet1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Hello World!", 50, 50);
+    }
+}
