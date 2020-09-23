8fd1cb95e6f8490f4e6cc2a20683f1966c9757b6
Misliv <may.hoang@student.rocvf.nl>
2020-09-23 10:00:50 +0200

    Applet 2.2 made, with white background

---
+++ src/h02/Applet2.java
@@ -0,0 +1,17 @@
+package h02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Applet2 extends Applet {
+    public void init() {
+        setBackground(Color.white);
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(May);
+        g.drawString(Hoang);
+
+    }
+}
