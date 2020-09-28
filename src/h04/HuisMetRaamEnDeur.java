2f205055385d538a01e9646f78e0690622e4fa06
Misliv <may.hoang@student.rocvf.nl>
2020-09-28 22:35:44 +0200

    square made

--- src/h04/HuisMetRaamEnDeur.java
+++ src/h04/HuisMetRaamEnDeur.java
@@ -1,4 +1,16 @@
 package h04;
 
-public class HuisMetRaamEnDeur {
+import java.applet.Applet;
+import java.awt.*;
+
+public class HuisMetRaamEnDeur extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawRect(75,100, 200, 175);
+
+    }
 }
