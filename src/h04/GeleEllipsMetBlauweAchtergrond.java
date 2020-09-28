27bead7fd45cb301e6523979781561fde96cc4ca
Misliv <may.hoang@student.rocvf.nl>
2020-09-29 00:45:44 +0200

    added blue background, red oval, yellow slice

--- src/h04/GeleEllipsMetBlauweAchtergrond.java
+++ src/h04/GeleEllipsMetBlauweAchtergrond.java
@@ -1,4 +1,20 @@
 package h04;
 
-public class GeleEllipsMetBlauweAchtergrond {
+import java.applet.Applet;
+import java.awt.*;
+
+public class GeleEllipsMetBlauweAchtergrond extends Applet {
+
+    public void init() {
+        setBackground(Color.BLUE);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillOval(50, 75, 300,150);
+        g.setColor(Color.yellow);
+        g.fillArc(50,75,300,150,50,75);
+
+//niet zeker of dit de opdracht was
+    }
 }
