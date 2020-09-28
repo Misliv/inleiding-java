46487cb048c15b133d4c85a22987ab8f8257dfe5
Misliv <may.hoang@student.rocvf.nl>
2020-09-28 22:55:38 +0200

    left window added and sorted code

--- src/h04/HuisMetRaamEnDeur.java
+++ src/h04/HuisMetRaamEnDeur.java
@@ -12,8 +12,11 @@ public class HuisMetRaamEnDeur extends Applet {
     public void paint(Graphics g) {
         //square house
         g.drawRect(100,125, 200, 150);
+        //rectangle door
         g.drawRect(185,215, 30,60);
-
+        //left window
+        g.drawRect(125,175,40,40);
+        //right window
         //roof
 
 
