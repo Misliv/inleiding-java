c705385246f9f64998fadd9ea0d93f76153576d4
Misliv <may.hoang@student.rocvf.nl>
2020-09-28 23:48:45 +0200

    general diagram made and sorted code

--- src/h04/StaafDiagram.java
+++ src/h04/StaafDiagram.java
@@ -1,4 +1,24 @@
 package h04;
 
-public class StaafDiagram {
+import java.applet.Applet;
+import java.awt.*;
+
+public class StaafDiagram extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        //diagram
+        g.drawLine(75,50,75,275);
+        g.drawLine(75,275,325,275);
+        //Valerie
+        g.drawRect(100, 10,51,265);
+        //Jeroen
+        g.drawRect(176, 10,51,265);
+        //Hans
+        g.drawRect(252, 10,51,265);
+
+    }
 }
