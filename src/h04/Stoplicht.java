8523f4dedce154bc2fce6a22113ac3b2620557de
Misliv <may.hoang@student.rocvf.nl>
2020-09-29 01:20:03 +0200

    added red and yellow light, yellow is in red light, ill fix later

--- src/h04/Stoplicht.java
+++ src/h04/Stoplicht.java
@@ -13,9 +13,15 @@ public class Stoplicht extends Applet {
         g.fillOval(150, 50, 100, 75);
         g.fillRect(150, 85, 100, 125);
         g.fillOval(150,175,100,75);
+        g.fillRect(190, 200, 20, 300);
         //red light
+        g.setColor(Color.RED);
+        g.fillOval(180, 70, 40, 40);
         //yellow light
+        g.setColor(Color.yellow);
+        g.fillOval(180, 85, 40, 40);
         //green light
+        g.setColor(Color.GREEN);
 
 //dunno why its uneven on the bottom
     }
