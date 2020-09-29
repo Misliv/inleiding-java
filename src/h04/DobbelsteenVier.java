39aa56849b5f8ea1099e328cddea7f80215ee87d
Misliv <may.hoang@student.rocvf.nl>
2020-09-29 04:49:53 +0200

    square made + 2 new lines

--- src/h04/DobbelsteenVier.java
+++ src/h04/DobbelsteenVier.java
@@ -11,7 +11,11 @@ public class DobbelsteenVier extends Applet {
         //zijde 4
         g.drawLine(75,100,75,250);
         g.drawLine(250,120,250,270);
+        g.drawLine(75,100,250,120);
+        g.drawLine(75,250,250,270);
         //zijde 5
+        g.drawLine(250,120,325,100);
+        g.drawLine(250,270,325, 250);
         //zijde 6
 
     }
