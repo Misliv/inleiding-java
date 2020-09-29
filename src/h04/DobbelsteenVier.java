d974bf1521d7dddb8f33b778672ecabbe8accde9
Misliv <may.hoang@student.rocvf.nl>
2020-09-29 04:40:50 +0200

    sorted and has two lines

--- src/h04/DobbelsteenVier.java
+++ src/h04/DobbelsteenVier.java
@@ -1,6 +1,18 @@
 package h04;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class DobbelsteenVier extends Applet {
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+        //zijde 4
+        g.drawLine(75,100,75,250);
+        g.drawLine(250,120,250,270);
+        //zijde 5
+        //zijde 6
+
+    }
 }
