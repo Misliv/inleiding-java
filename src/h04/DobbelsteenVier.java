fdaeed113e2fbaff69fa4e05282de29e18cc4ee5
Misliv <may.hoang@student.rocvf.nl>
2020-09-29 17:59:25 +0200

    made a cube + added an oval

--- src/h04/DobbelsteenVier.java
+++ src/h04/DobbelsteenVier.java
@@ -9,14 +9,32 @@ public class DobbelsteenVier extends Applet {
     }
     public void paint(Graphics g) {
         //zijde 4
-        g.drawLine(75,100,75,250);
-        g.drawLine(250,120,250,270);
-        g.drawLine(75,100,250,120);
-        g.drawLine(75,250,250,270);
+        g.drawLine(100,90,100,240);
+        g.drawLine(200,120,200,270);
+        g.drawLine(100,90,200,120);
+        g.drawLine(100,240,200,270);
+        g.fillOval(105,105,25,35);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
         //zijde 5
-        g.drawLine(250,120,325,100);
-        g.drawLine(250,270,325, 250);
+        g.drawLine(200,120,315,100);
+        g.drawLine(200,270,315, 250);
+        g.drawLine(315,100,315,250);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
         //zijde 6
+        g.drawLine(100,90,215,70);
+        g.drawLine(215,70,315,100);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
+        g.fillOval(10,10,10,10);
 
     }
 }
