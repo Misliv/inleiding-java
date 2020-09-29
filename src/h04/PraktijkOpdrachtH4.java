df100b03be06ed97d6009e6127f006009dc62c51
Misliv <may.hoang@student.rocvf.nl>
2020-09-30 00:44:32 +0200

    'Gevulde rechthoek met ovaal' made

--- src/h04/PraktijkOpdrachtH4.java
+++ src/h04/PraktijkOpdrachtH4.java
@@ -20,5 +20,10 @@ public class PraktijkOpdrachtH4 extends Applet {
         g.drawString("Afgeronde rechthoek", 82, 320);
         g.drawRoundRect(25,200,225,100,30,30);
 
+        //Gevulde rechthoek met ovaal
+        g.drawString("Gevulde rechthoek met ovaal", 293, 180);
+        g.setColor(Color.MAGENTA);
+        g.fillRect(260,60,225,100);
+
     }
 }
