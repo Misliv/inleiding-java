f117a699fbaf1da148c3d0fef1c101fcc29d1f23
Misliv <may.hoang@student.rocvf.nl>
2020-09-30 00:29:53 +0200

    'Lijn' made

---
+++ src/h04/PraktijkOpdrachtH4.java
@@ -0,0 +1,16 @@
+package h04;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class PraktijkOpdrachtH4 extends Applet {
+    public void init() {
+
+    }
+    public void paint(Graphics g) {
+        //Lijn
+        g.drawString("Lijn",130,40);
+        g.drawLine(25,20,250,20);
+
+    }
+}
