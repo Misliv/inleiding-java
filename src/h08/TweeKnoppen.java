7a898c4088fe11b83e4ed8f4f89fd161e022129f
Misliv <may.hoang@student.rocvf.nl>
2020-10-21 09:51:30 +0200

    added "Ok" and "Reset" button, and a textfield

--- src/h08/TweeKnoppen.java
+++ src/h08/TweeKnoppen.java
@@ -2,14 +2,40 @@ package h08;
 
 import java.applet.Applet;
 import java.awt.*;
+import java.awt.event.*;
 
 public class TweeKnoppen extends Applet {
+    Button Okknop;
+    Button Resetknop;
+    TextField Textfield;
+    Label label;
+    String schermtekst;
 
     public void init() {
-
+        schermtekst = "Add text";
+        Okknop = new Button("Ok");
+        KnopListener kl = new KnopListener();
+        Okknop.addActionListener( kl );
+        add(Okknop);
+        Resetknop = new Button("Reset");
+        Resetknop.addActionListener( kl );
+        add(Resetknop);
+        Textfield = new TextField("Type here", 20);
+        label = new Label("Type next to me!");
+        add(label);
+        add(Textfield);
     }
 
     public void paint(Graphics g) {
+        g.drawString(schermtekst, 50,50);
 
+    }
+
+    class KnopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            schermtekst = "it works!";
+            repaint();
+
+        }
     }
 }
