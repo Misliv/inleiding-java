da50b79eb241b503c126aec32e1a1d6e37c00de7
Misliv <may.hoang@student.rocvf.nl>
2020-10-23 16:39:31 +0200

    added variables and strings

--- src/h06/SecondenInUurDagJaar.java
+++ src/h06/SecondenInUurDagJaar.java
@@ -4,18 +4,26 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class SecondenInUurDagJaar extends Applet {
-    int seconde;
+    int a;
+    int b;
+    int c;
     int uur;
     int dag;
     int jaar;
-    int uitkomst;
 
     public void init() {
-
+        a = 60;
+        b = 24;
+        c = 365;
+        uur = a * a;
+        dag = uur * b;
+        jaar = dag * c;
 
     }
 
     public void paint(Graphics g) {
-
+        g.drawString("Seconden in een uur" + uur, 10, 20);
+        g.drawString("Seconden in een dag" + dag, 10,40);
+        g.drawString("Seconden in een jaar" + jaar,10,60);
     }
 }
