c5b35922c8bb2066e6a0b63af4ab6153a0a43f3f
Misliv <may.hoang@student.rocvf.nl>
2020-10-26 23:26:13 +0100

    made an equation

--- src/h06/AddPositiveMakeNegative.java
+++ src/h06/AddPositiveMakeNegative.java
@@ -1,4 +1,22 @@
 package h06;
 
-public class AddPositiveMakeNegative {
+import java.applet.Applet;
+import java.awt.*;
+
+public class AddPositiveMakeNegative extends Applet {
+    int maxnumber;
+    int number;
+    int uitkomst;
+
+    public void init() {
+        maxnumber = 2147483647;
+        number = 1;
+        uitkomst = 2147483647 + 1;
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("2147483647 + 1 = " + uitkomst,10,10);
+
+    }
 }
