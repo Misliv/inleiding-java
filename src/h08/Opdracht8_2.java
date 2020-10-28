d6cf8f53123e9183d48bcdc95a883d20bc5a6a39
Misliv <may.hoang@student.rocvf.nl>
2020-10-28 10:04:43 +0100

    init and paint added

--- src/h08/Opdracht8_2.java
+++ src/h08/Opdracht8_2.java
@@ -1,6 +1,15 @@
 package h08;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class Opdracht8_2 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
 }
