5ad87bcebdd6f57c8f45d00fb12c2873af68f30d
Misliv <may.hoang@student.rocvf.nl>
2020-10-28 01:57:50 +0100

    added ints + string

--- src/h06/PraktijkOpdrachtH6.java
+++ src/h06/PraktijkOpdrachtH6.java
@@ -1,4 +1,24 @@
 package h06;
 
-public class PraktijkOpdrachtH6 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class PraktijkOpdrachtH6 extends Applet {
+    int cijfer1;
+    int cijfer2;
+    int cijfer3;
+    int uitkomst;
+
+    public void init() {
+        cijfer1 = 5.9;
+        cijfer2 = 6.3;
+        cijfer3 = 6.9;
+        uitkomst = (cijfer1 + cijfer2 + cijfer3) / 3;
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Het gemiddelde is: " + uitkomst,50,50);
+
+    }
 }
