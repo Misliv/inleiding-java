0bab0a0e698319dc6cdeb77e6691d51d7de5f53e
Misliv <may.hoang@student.rocvf.nl>
2020-11-04 09:52:02 +0100

    if statement added

--- src/h10/Opdracht10_1.java
+++ src/h10/Opdracht10_1.java
@@ -1,4 +1,35 @@
 package h10;
 
-public class Opdracht10_1 {
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Opdracht10_1 extends Applet{
+    TextField nummerveld;
+    String nummer;
+    double hoogstegetal;
+    double getal;
+
+    public void init() {
+        nummerveld = new TextField("");
+        nummerveld.addActionListener( new FieldListener());
+        nummer = "";
+    }
+
+    public void paint(Graphics g) {
+    }
+    class FieldListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String s;
+
+            s = nummerveld.getText();
+            getal = Integer.parseInt(s);
+            if (hoogstegetal > getal) {
+                nummer = hoogstegetal;
+                repaint();
+            }
+
+        }
+    }
 }
