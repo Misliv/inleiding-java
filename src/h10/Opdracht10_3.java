89b7da638751df99eb46923749a27f77ca96919d
Misliv <may.hoang@student.rocvf.nl>
2020-11-05 09:19:17 +0100

    added correct number of days in a month, the text appears now, everything works as it should

--- src/h10/Opdracht10_3.java
+++ src/h10/Opdracht10_3.java
@@ -7,18 +7,22 @@ import java.awt.event.ActionListener;
 
 public class Opdracht10_3 extends Applet {
     TextField tekstvak;
+    Label label;
     String s, text;
     int maand;
 
     public void init() {
         tekstvak = new TextField("",10);
+        label = new Label("Give a number from 1-12 it and press enter");
         tekstvak.addActionListener( new TekstvakListener());
         text = "";
+        add(label);
         add(tekstvak);
 
     }
 
     public void paint(Graphics g) {
+        g.drawString(text,50,50);
 
     }
     class TekstvakListener implements ActionListener {
@@ -30,19 +34,19 @@ public class Opdracht10_3 extends Applet {
                     text = "Januari, 31";
                     break;
                 case 2:
-                    text = "Februari, 28";
+                    text = "Februari, 28 of 29";
                     break;
                 case 3:
                     text = "Maart, 31";
                     break;
                 case 4:
-                    text = "April, 31";
+                    text = "April, 30";
                     break;
                 case 5:
                     text = "Mei, 31";
                     break;
                 case 6:
-                    text = "Juni, 31";
+                    text = "Juni, 30";
                     break;
                 case 7:
                     text = "Juli, 31";
@@ -51,18 +55,22 @@ public class Opdracht10_3 extends Applet {
                     text = "Augustus, 31";
                     break;
                 case 9:
-                    text = "September, 31";
+                    text = "September, 30";
                     break;
                 case 10:
                     text = "Oktober, 31";
                     break;
                 case 11:
-                    text = "November, 31";
+                    text = "November, 30";
                     break;
                 case 12:
                     text = "December, 31";
                     break;
+                default:
+                    text = "Give a number from 1-12";
+                    break;
             }
+            repaint();
         }
     }
 }
