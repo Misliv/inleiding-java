dab0c8be0131f9bcc55a1a20c76689b65f273a63
Misliv <may.hoang@student.rocvf.nl>
2020-11-11 10:46:19 +0100

    added gemiddelde, voldoende of onvoldoende

--- src/h10/Opdracht10_5.java
+++ src/h10/Opdracht10_5.java
@@ -6,36 +6,48 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Opdracht10_5 extends Applet {
-    int onvoldoende;
-    int voldoende;
+    double invoercijfer;
+    double totaal;
+    double aantalcijfer;
+    double gemiddelde;
     TextField tekstvak;
     Label label;
     String tekst;
+    String s;
     Button Okknop;
 
     public void init() {
         tekstvak = new TextField(" ", 5);
-        tekstvak.addActionListener( new TekstListener());
         tekst = "";
-        label = new Label ("Input grades");
+        label = new Label ("Voer in cijfers");
         Okknop = new Button("Ok");
-        Okknop.addActionListener(new KnopListener());
+        Okknop.addActionListener(new TekstListener());
         add (label);
         add (tekstvak);
+        add (Okknop);
+
     }
     public void paint(Graphics g) {
+        g.drawString(tekst, 50,50);
+        g.drawString(gemiddelde + "",50,60);
+
     }
     class TekstListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            onvoldoende = Integer.parseInt(tekst);
-            if (onvoldoende < 5.5)
+            s = tekstvak.getText();
+        invoercijfer = Double.parseDouble(s);
+            if (invoercijfer >= 5.5){
+                tekst = "Voldoende";
+        }
+            else {
                 tekst = "Onvoldoende";
-
-        }
-        class KnopListener implements ActionListener {
-            public void actionPerformed(ActionEvent e) {
-
             }
+            totaal += invoercijfer;
+            aantalcijfer ++;
+            gemiddelde = totaal / aantalcijfer;
+            repaint();
+
         }
-    }
-}
+        }
+            }
+
