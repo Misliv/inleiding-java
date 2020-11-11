9641e697564d45c4bde8d89f12e7c3a187942bf0
Misliv <may.hoang@student.rocvf.nl>
2020-11-11 12:13:23 +0100

    while statement, it counts down starting from 20 ending at 11 showing 10 numbers

--- src/h11/Opdracht11_2.java
+++ src/h11/Opdracht11_2.java
@@ -1,6 +1,21 @@
 package h11;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class Opdracht11_2 extends Applet {
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        int teller = 20;
+        int y = 0;
+
+        while(teller > 10) {
+            y += 20;
+            g.drawString("" + teller, 50, y);
+            teller--;
+        }
+    }
 }
