65064d3db4388d1101388c0dea9eced6e42d739d
Misliv <may.hoang@student.rocvf.nl>
2020-11-12 17:20:11 +0100

    loop gives 50 circles

--- src/h11/Opdracht11_7.java
+++ src/h11/Opdracht11_7.java
@@ -5,16 +5,17 @@ import java.awt.*;
 
 public class Opdracht11_7 extends Applet {
     public void init() {
+        setSize(1000,1000);
     }
 
     public void paint(Graphics g) {
         int teller = 0;
-        int width = 10;
-        int height = 10;
-        int x = 200;
-        int y = 150;
+        int width = 2;
+        int height = 2;
+        int x = 400;
+        int y = 300;
 
-        while (teller <= 4) {
+        while (teller <= 50) {
             width += 10;
             height += 10;
             x -= 5;
