5ae2c2a13d9a37616b035008ffed778366598f5d
Misliv <may.hoang@student.rocvf.nl>
2020-11-12 17:33:02 +0100

    loop gives 100 circles that get larger on the same spot

--- src/h11/Opdracht11_8.java
+++ src/h11/Opdracht11_8.java
@@ -1,6 +1,26 @@
 package h11;
 
 import java.applet.Applet;
+import java.awt.*;
 
 public class Opdracht11_8 extends Applet {
+    public void init() {
+        setSize(1000,1000);
+    }
+
+    public void paint(Graphics g) {
+        int teller = 0;
+        int width = 2;
+        int height = 2;
+        int x = 10;
+        int y = 10;
+
+        while (teller <= 100) {
+            width += 10;
+            height += 10;
+            g.drawOval(x, y, width, height);
+            teller++;
+
+        }
+    }
 }
