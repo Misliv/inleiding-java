17cf2660cdb8d1922edbeb0c44b5bceb0d407cba
Misliv <may.hoang@student.rocvf.nl>
2020-11-18 07:04:59 +0100

    chessboard thats 10x10

--- src/h11/Opdracht11_9.java
+++ src/h11/Opdracht11_9.java
@@ -4,11 +4,23 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Opdracht11_9 extends Applet {
+    static int N = 10;
 
-    public void init() {
+    // Use paint() method
+    public void paint(Graphics g)
+    {
+        int x, y;
+        for (int rij = 0; rij < N; rij++) {
 
-    }
-
-    public void paint(Graphics g) {
+            for (int kolom = 0; kolom < N; kolom++) {
+                x = rij * 20;
+                y = kolom * 20;
+                if ((rij % 2 == 0) == (kolom % 2 == 0))
+                    g.setColor(Color.BLACK);
+                else
+                    g.setColor(Color.WHITE);
+                g.fillRect(x, y, 20, 20);
+            }
+        }
     }
 }
---
+++ src/h11/PraktijkOpdracht2H11.java
@@ -0,0 +1,6 @@
+package h11;
+
+import java.applet.Applet;
+
+public class PraktijkOpdracht2H11 extends Applet {
+}
