1b6054d0dd3777f782481d1c2bfaf1cb15f3c331
Misliv <may.hoang@student.rocvf.nl>
2020-11-25 14:15:22 +0100

    for loops made, new int, sorts numbers now

--- src/h12/Opdracht12_3.java
+++ src/h12/Opdracht12_3.java
@@ -7,28 +7,37 @@ import java.awt.event.ActionListener;
 import java.util.*;
 
 public class Opdracht12_3 extends Applet {
-    TextField[] tekstvak;
+    TextField[] tekstvak = new TextField[5];
+    int[] getal = new int[5];
     Button okknop;
 
     public void init() {
         okknop = new Button("Ok");
         okknop.addActionListener(new OkknopListener());
         add(okknop);
-        tekstvak = new TextField[5];
         for (int teller = 0; teller < tekstvak.length; teller++) {
-            tekstvak[teller] = new TextField(" ");
+            tekstvak[teller] = new TextField("");
             add(tekstvak[teller]);
+
         }
-       // Arrays.sort(tekstvak);
+
     }
-        public void paint (Graphics g){
-        }
+
+    public void paint(Graphics g) {
     }
-      class OkknopListener implements ActionListener {
+
+    class OkknopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-          // TextField[].setText("");
-          //  repaint();
+            for (int teller = 0; teller < tekstvak.length; teller++) {
+                getal[teller] = Integer.parseInt(tekstvak[teller].getText());
+            }
+            Arrays.sort(getal);
+            for (int teller = 0; teller < tekstvak.length; teller++) {
+                tekstvak[teller].setText(getal[teller] + "");
+            }
+            repaint();
         }
+    }
 }
 
 
