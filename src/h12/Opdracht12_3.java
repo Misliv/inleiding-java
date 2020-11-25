0d8f2a663323cec33346ba44b336ca0a7b934512
Misliv <may.hoang@student.rocvf.nl>
2020-11-25 09:28:55 +0100

    textfield arrays added and a for loop

--- src/h12/Opdracht12_3.java
+++ src/h12/Opdracht12_3.java
@@ -5,11 +5,17 @@ import java.awt.*;
 import java.util.*;
 
 public class Opdracht12_3 extends Applet {
-    TextField[] tekstvak = {};
+    TextField[] tekstvak;
+
     public void init() {
+        tekstvak = new TextField[5];
+        for (int teller = 0; teller < tekstvak.length; teller++)
+            tekstvak[teller] = new TextField();
+        add(tekstvak[teller]);
+        Arrays.sort(tekstvak);
     }
 
     public void paint(Graphics g) {
-        Arrays.sort(tekstvak);
+
     }
 }
