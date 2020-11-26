c72e4eb56da8567829e0865db4f62c280e735e7c
Misliv <may.hoang@student.rocvf.nl>
2020-11-26 13:09:45 +0100

    for loops, if statements added

--- src/h12/Opdracht12_4.java
+++ src/h12/Opdracht12_4.java
@@ -6,28 +6,47 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Opdracht12_4 extends Applet {
-    int[] getal = new int[1];
+    double[] getal = {14.0, 16.0, 2.0, 7.0, 3.0, 21.0};
+    Boolean gevonden;
     TextField tekstvak;
     Button okknop;
+    double gezocht;
 
     public void init() {
         okknop = new Button("Ok");
         okknop.addActionListener(new OkknopListener());
-        tekstvak = new TextField("", 5);
+        tekstvak = new TextField("", 20);
         add(okknop);
         add(tekstvak);
-        for (int teller = 0; teller < getal.length; teller++) {
+        {
+            for (int teller = 0; teller < getal.length; teller++) {
+                gezocht = getal.length;
+                gevonden = false;
+            }
+        }
+        int teller = 0;
+        while (teller < getal.length) {
+            if (getal[teller] == gezocht) {
+                gevonden = true;
+            }
+            teller++;
         }
     }
     public void paint(Graphics g) {
 
-            }
-            class OkknopListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            tekstvak.setText("");
-            repaint();
-            for (int teller = 0; teller < getal.length; teller++);
-        }
+    }
 
+    class OkknopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (gevonden == true) {
+                tekstvak.setText("De waarde is gevonden, index = " + getal.length);
+                repaint();
+            }
+
+            else {
+                tekstvak.setText("De waarde is niet gevonden.");
+            }
+                repaint();
+        }
     }
 }
