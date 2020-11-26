87b922d1f4fd3d5dd1e8519fa238b18ca02cf862
Misliv <may.hoang@student.rocvf.nl>
2020-11-26 13:14:59 +0100

    copied 12.4 to 12.6

--- src/h12/Opdracht12_6.java
+++ src/h12/Opdracht12_6.java
@@ -1,15 +1,52 @@
 package h12;
 
+import java.applet.Applet;
 import java.awt.*;
-import java.applet.*;
-import java.util.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Opdracht12_6 extends Applet {
+    double[] getal = {14.0, 16.0, 2.0, 7.0, 3.0, 21.0};
+    Boolean gevonden;
+    TextField tekstvak;
+    Button okknop;
+    double gezocht;
 
     public void init() {
+        okknop = new Button("Ok");
+        okknop.addActionListener(new Opdracht12_4.OkknopListener());
+        tekstvak = new TextField("", 20);
+        add(okknop);
+        add(tekstvak);
+        {
+            for (int teller = 0; teller < getal.length; teller++) {
+                gezocht = getal.length;
+                gevonden = false;
+            }
+        }
+        int teller = 0;
+        while (teller < getal.length) {
+            if (getal[teller] == gezocht) {
+                gevonden = true;
+            }
+            teller++;
+        }
+    }
+    public void paint(Graphics g) {
+
     }
 
-    public void paint(Graphics g) {
+    class OkknopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (gevonden == true) {
+                tekstvak.setText("De waarde is gevonden, index = " + getal.length);
+                repaint();
+            }
+
+            else {
+                tekstvak.setText("De waarde is niet gevonden.");
+            }
+            repaint();
+        }
     }
 }
