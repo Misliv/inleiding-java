3e87e0f30d56d1342c2709632a3b11f1137ec5c4
Misliv <may.hoang@student.rocvf.nl>
2020-11-30 21:36:43 +0100

    textfield and button

--- src/h12/Opdracht12_6.java
+++ src/h12/Opdracht12_6.java
@@ -1,52 +1,35 @@
 package h12;
 
+import javax.xml.soap.Text;
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Opdracht12_6 extends Applet {
-    double[] getal = {14.0, 16.0, 2.0, 7.0, 3.0, 21.0};
-    Boolean gevonden;
+public class Opdracht12_6 extends Applet implements ActionListener {
+    double[] getal = {1, 1, 1, 1, 2, 3, 3, 3, 4};
+    double nummer;
     TextField tekstvak;
-    Button okknop;
-    double gezocht;
+    Button button;
 
     public void init() {
-        okknop = new Button("Ok");
-        okknop.addActionListener(new OkknopListener());
-        tekstvak = new TextField("", 20);
-        add(okknop);
+        button = new Button("oalright");
+        button.addActionListener(this);
+        tekstvak = new TextField("", 5);
         add(tekstvak);
-        {
-            for (int teller = 0; teller < getal.length; teller++) {
-                gezocht = getal.length;
-                gevonden = false;
-            }
-        }
-        int teller = 0;
-        while (teller < getal.length) {
-            if (getal[teller] == gezocht) {
-                gevonden = true;
-            }
-            teller++;
-        }
+        add(button);
+
     }
+
     public void paint(Graphics g) {
 
     }
 
-    class OkknopListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            if (gevonden == true) {
-                tekstvak.setText("De waarde is gevonden, index = " + getal.length);
-                repaint();
-            }
 
-            else {
-                tekstvak.setText("De waarde is niet gevonden.");
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            nummer = Double.parseDouble(tekstvak.getText());{
             }
-            repaint();
         }
     }
 }
