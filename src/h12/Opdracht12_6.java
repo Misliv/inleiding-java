942b8a74d31fad845660502d4661fee90f207858
Misliv <may.hoang@student.rocvf.nl>
2020-11-30 23:01:56 +0100

    applet shows value and gives the amount of times it shows up

--- src/h12/Opdracht12_6.java
+++ src/h12/Opdracht12_6.java
@@ -7,13 +7,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Opdracht12_6 extends Applet implements ActionListener {
-    double[] getal = {1, 1, 1, 1, 2, 3, 3, 3, 4};
-    double nummer;
+    double[] getal = {1, 1, 1, 1, 2, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 20, 20, 20};
+    double gezocht;
+    int count;
     TextField tekstvak;
     Button button;
 
+
     public void init() {
-        button = new Button("oalright");
+        button = new Button("Ok");
         button.addActionListener(this);
         tekstvak = new TextField("", 5);
         add(tekstvak);
@@ -22,14 +24,21 @@ public class Opdracht12_6 extends Applet implements ActionListener {
     }
 
     public void paint(Graphics g) {
+        g.drawString("Waarde: " + gezocht,50,50);
+        g.drawString("Aantal keer laten zien: " + count,50,70);
 
     }
 
-
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
-            nummer = Double.parseDouble(tekstvak.getText());{
+            gezocht = Double.parseDouble(tekstvak.getText());
+            count = 0;
+            for(int teller = 0; teller < getal.length; teller++) {
+                if (gezocht == getal[teller]) {
+                    count++;
+                }
             }
+            repaint();
         }
     }
 }
