b6ee2d9f582c69f81fac058064a4eb3656eea671
Misliv <may.hoang@student.rocvf.nl>
2020-12-02 08:58:16 +0100

    3 drawlines that line up to make a triangle

--- src/h13/Opdracht13_1.java
+++ src/h13/Opdracht13_1.java
@@ -11,8 +11,12 @@ public class Opdracht13_1 extends Applet {
     }
 
     public void paint(Graphics g) {
+        tekenDriehoek(g, 200, 125, 100,250,300,250);
 
     }
     void tekenDriehoek( Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
+        g.drawLine(x1, y1, x2, y2);
+        g.drawLine(x2, y2, x3, y3);
+        g.drawLine(x3, y3, x1, y1);
     }
 }
