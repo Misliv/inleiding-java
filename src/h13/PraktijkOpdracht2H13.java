78ceee7e3255daed636dcc6c185005e3b4e18c72
Misliv <may.hoang@student.rocvf.nl>
2020-12-02 11:25:14 +0100

    seperated tekenBlad and tekenStam

--- src/h13/PraktijkOpdracht2H13.java
+++ src/h13/PraktijkOpdracht2H13.java
@@ -4,15 +4,33 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class PraktijkOpdracht2H13 extends Applet {
+    int boom = 7;
+
     public void init() {
     }
+
     public void paint(Graphics g) {
-        tekenBoom(g,175,150, 20,80,150,100,70,70);
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        for (int rij = 1; rij < boom; rij++) {
+            for (int kolom = 1; kolom < boom; kolom++) {
+                x1 = rij * 50;
+                y1 = kolom * 60;
+                x2 = rij * 60;
+                y2 = kolom * 50;
+                if ((rij % 2 == 0) == (kolom % 2 == 0))
+                    tekenBoom(g, x1, y1, 20, 80, x2, y2, 70, 70);
+            }
+        }
     }
-    void tekenBoom(Graphics g, int x1, int y1, int width1, int height1, int x2, int y2,  int width2, int height2) {
+    void tekenBoom (Graphics g,int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2){
         g.setColor(Color.GRAY);
         g.fillRect(x1, y1, width1, height1);
         g.setColor(Color.GREEN);
         g.fillOval(x2, y2, width2, height2);
     }
 }
+
+
--- src/h13/PraktijkOpdrachtH13.java
+++ src/h13/PraktijkOpdrachtH13.java
@@ -7,12 +7,18 @@ public class PraktijkOpdrachtH13 extends Applet {
     public void init() {
     }
     public void paint(Graphics g) {
-        tekenBoom(g,175,150, 20,80,150,100,70,70);
+        tekenStam(g,175,150, 20,80);
+        tekenBlad(g,150,100,70,70);
+
     }
-    void tekenBoom(Graphics g, int x1, int y1, int width1, int height1, int x2, int y2,  int width2, int height2) {
-        g.setColor(Color.GRAY);
-        g.fillRect(x1, y1, width1, height1);
+    void tekenBlad(Graphics g, int x1, int y1, int width1, int height1) {
         g.setColor(Color.GREEN);
-        g.fillOval(x2, y2, width2, height2);
+        g.fillOval(x1, y1, width1, height1);
     }
+    void tekenStam(Graphics g, int x2, int y2,  int width2, int height2) {
+        g.setColor(Color.GRAY);
+        g.fillRect(x2, y2, width2, height2);
+    }
+
+
 }
