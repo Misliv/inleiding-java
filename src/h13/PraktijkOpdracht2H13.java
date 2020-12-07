4d2f2eeaf8e2d937e82dfd637823e95c03f42e84
Misliv <may.hoang@student.rocvf.nl>
2020-12-07 22:42:57 +0100

    trees, kinda

--- src/h13/PraktijkOpdracht2H13.java
+++ src/h13/PraktijkOpdracht2H13.java
@@ -4,10 +4,10 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class PraktijkOpdracht2H13 extends Applet {
-    int blad = 7;
-    int stam = 7;
+    int boom = 10;
 
     public void init() {
+        setSize(1500,1500);
     }
 
     public void paint(Graphics g) {
@@ -15,30 +15,21 @@ public class PraktijkOpdracht2H13 extends Applet {
         int y1;
         int x2;
         int y2;
-        for (int rij = 1; rij < blad; rij++) {
-            for (int kolom = 1; kolom < blad; kolom++) {
-                x1 = rij * 70;
-                y1 = kolom * 60;
-                if ((rij % 2 == 0) == (kolom % 2 == 0))
-                    tekenBlad(g, x1, y1, 70, 70);
-            for (rij = 1; rij < stam; rij++) {
-                for (kolom = 1; kolom < stam; kolom++) {
-                    x2 = rij * 60;
-                    y2 = kolom * 50;
-                    if ((rij % 2 == 0) == (kolom % 2 == 0))
-                tekenStam(g, x2, y2, 20, 80);
-                }
-            }
+        for (int rij = 1; rij < boom; rij++) {
+            for (int kolom = 1; kolom < boom; kolom++) {
+                x2 = rij * 130;
+                y2 = kolom * 150;
+                x1 = rij * 120;
+                y1 = kolom * 130;
+                tekenBoom(g, x1, y1, 80,50, x2, y2,20,80);
             }
         }
     }
-    void tekenBlad (Graphics g,int x1, int y1, int width1, int height1){
-        g.setColor(Color.GREEN);
-        g.fillOval(x1, y1, width1, height1);
-    }
-    void tekenStam (Graphics g,int x2, int y2, int width2, int height2){
+    void tekenBoom(Graphics g, int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2) {
         g.setColor(Color.GRAY);
         g.fillRect(x2, y2, width2, height2);
+        g.setColor(Color.GREEN);
+        g.fillOval(x1, y1, width1, height1);
     }
 }
 
