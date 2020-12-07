f740f58a4a9b59d505ac8fb602e405f0fe44e352
Misliv <may.hoang@student.rocvf.nl>
2020-12-07 18:22:51 +0100

    made a tree

--- src/h13/PraktijkOpdrachtH13.java
+++ src/h13/PraktijkOpdrachtH13.java
@@ -7,16 +7,15 @@ public class PraktijkOpdrachtH13 extends Applet {
     public void init() {
     }
     public void paint(Graphics g) {
-        tekenStam(g,175,150, 20,80);
-        tekenBlad(g,150,100,70,70);
+        tekenBoom(g,145,100, 80,80, 175,150,20,80);
+
 
     }
-    void tekenBlad(Graphics g, int x1, int y1, int width1, int height1) {
-        g.setColor(Color.GREEN);
-        g.fillOval(x1, y1, width1, height1);
-    }
-    void tekenStam(Graphics g, int x2, int y2,  int width2, int height2) {
+    void tekenBoom(Graphics g, int x1, int y1, int width1, int height1, int x2, int y2, int width2, int height2) {
         g.setColor(Color.GRAY);
         g.fillRect(x2, y2, width2, height2);
+        g.setColor(Color.GREEN);
+        g.fillOval(x1, y1, width1, height1);
+
     }
 }
